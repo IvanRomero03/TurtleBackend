1a9823903c3d994746e9435170a637b4a832cabb
IvanRomero03 <i.wells.ar@gmail.com>
2023-03-13 17:37:57 -0600

    Send SVG

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import json
+from Parser import Parser
+from RedisDB import RedisDB
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -16,17 +18,29 @@ class handler(BaseHTTPRequestHandler):
         textInput = text["text"]
         print(textInput)
         print(text)
+        parser = Parser()
+        result = parser.parse(textInput)
+        parser.execute()
+        parser.save("temp.svg")
+
         response = 200
         self.send_response(response)
-        self.send_header('Content-type','text/html')
+        # send the svg file
+        self.send_header('Content-type','image/svg+xml')
         self.end_headers()
-        self.wfile.write(bytes("Hello World !", "utf8"))
+        with open("temp.svg", "rb") as f:
+            self.wfile.write(f.read())
+        return
+
+        # self.send_header('Content-type','text/html')
+        # self.end_headers()
+        # self.wfile.write(bytes("Hello World !", "utf8"))
         return
     
 
-# if __name__ == '__main__':
-#     from http.server import HTTPServer
-#     server = HTTPServer(('localhost', 8080), handler)
-#     print('Starting server, use <Ctrl-C> to stop')
-#     server.serve_forever()
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('localhost', 8080), handler)
+    print('Starting server, use <Ctrl-C> to stop')
+    server.serve_forever()
 
