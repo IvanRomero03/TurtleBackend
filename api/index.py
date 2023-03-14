842834fb492b0e37b7ca3afd09577f36c6cca6f2
IvanRomero03 <i.wells.ar@gmail.com>
2023-03-13 19:55:55 -0600

    singleton handlers and redis conn

--- api/index.py
+++ api/index.py
@@ -2,31 +2,26 @@ from http.server import BaseHTTPRequestHandler
 #from urllib import parse
 import json
 from .src.Parser import Parser
+from .src.handler import handler as SingletonHandler
+from .src.util import randomHash
 import os
 #from RedisDB import RedisDB
 
-class handler(BaseHTTPRequestHandler):
+class handler(SingletonHandler):
     def do_POST(self):
         s = self.path
-        print(s)
         # Content-Type: application/json
         # {"text": "fd 100"}
         text = self.rfile.read(int(self.headers['Content-Length'])).decode("utf-8")
-        textJson = json.dumps(text)
-        print(text)
         text = json.loads(text)
-        print(text)
         textInput = text["text"]
-        print(textInput)
-        print(text)
         parser = Parser()
         result = parser.parse(textInput)
         parser.execute()
-        print(os.getcwd())
-        # print tree 
-        print(os.listdir())
-        #create empty file
-        #open("/temp/temp.svg", "w").close()
+        # pass the result to the redis db 
+        # create a random hash
+        hash = randomHash(16)
+        self.redis.set(hash, str(result))
         svg = parser.getSVG()
 
         response = 200
@@ -37,15 +32,54 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(svg.encode("utf-8"))
         return
 
-        # self.send_header('Content-type','text/html')
-        # self.end_headers()
-        # self.wfile.write(bytes("Hello World !", "utf8"))
-        return
+
+
+# class handler(BaseHTTPRequestHandler):
+#     def do_POST(self):
+#         s = self.path
+#         print(s)
+#         # Content-Type: application/json
+#         # {"text": "fd 100"}
+#         text = self.rfile.read(int(self.headers['Content-Length'])).decode("utf-8")
+#         textJson = json.dumps(text)
+#         print(text)
+#         text = json.loads(text)
+#         print(text)
+#         textInput = text["text"]
+#         print(textInput)
+#         print(text)
+#         parser = Parser()
+#         result = parser.parse(textInput)
+#         parser.execute()
+#         print(os.getcwd())
+#         # print tree 
+#         print(os.listdir())
+#         #create empty file
+#         #open("/temp/temp.svg", "w").close()
+#         svg = parser.getSVG()
+
+#         response = 200
+#         self.send_response(response)
+#         # send the svg file
+#         self.send_header('Content-type','image/svg+xml')
+#         self.end_headers()
+#         self.wfile.write(svg.encode("utf-8"))
+#         return
+
+#         # self.send_header('Content-type','text/html')
+#         # self.end_headers()
+#         # self.wfile.write(bytes("Hello World !", "utf8"))
+#         return
     
+
+# if __name__ == '__main__':
+#     from http.server import HTTPServer
+#     server = HTTPServer(('localhost', 8080), handler)
+#     print('Starting server, use <Ctrl-C> to stop')
+#     server.serve_forever()
 
 if __name__ == '__main__':
     from http.server import HTTPServer
     server = HTTPServer(('localhost', 8080), handler)
     print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
-
---
+++ api/src/handler.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler
+import json
+from src.Parser import Parser
+import os
+from src.RedisDB import RedisDB
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Singleton implementation for handler class
+class handler(BaseHTTPRequestHandler):
+    def __new__(cls, *args) -> BaseHTTPRequestHandler:
+        print("new", cls)
+        print("new", args)
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(handler, cls).__new__(cls)
+        if not hasattr(cls, 'redis'):
+            cls.redis = RedisDB(os.getenv("REDIS_HOST"), os.getenv("REDIS_PORT"), os.getenv("REDIS_DB"), os.getenv("REDIS_USERNAME"), os.getenv("REDIS_PASSWORD"))
+        return cls.instance
+    
+    # def __init__(self) -> None:
+    #     #print("init", args)
+    #     super().__init__()
+        
+    #     return
+    
