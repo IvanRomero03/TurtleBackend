8031bba53d7e63367f4f79cf0c80f2e019cc9c06
IvanRomero03 <i.wells.ar@gmail.com>
2023-03-13 20:51:30 -0600

    srry

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET():
-        return "Hola"
+    def do_GET(self):
+        response = 200
+        self.send_response(response)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes("Hello World !", "utf8"))
+        return 
