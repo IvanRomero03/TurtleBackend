de659385d914d4efb7d2fcd53b48076494479951
IvanRomero03 <i.wells.ar@gmail.com>
2023-03-13 19:22:54 -0600

    aver

--- api/index.py
+++ api/index.py
@@ -23,6 +23,7 @@ class handler(BaseHTTPRequestHandler):
         result = parser.parse(textInput)
         parser.execute()
         print(os.getcwd())
+        # print tree
         #create empty file
         #open("/temp/temp.svg", "w").close()
         parser.save("temp/temp.svg")
