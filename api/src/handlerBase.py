948490f9d3c747109f18e71a096bbffaa086d9ff
IvanRomero03 <i.wells.ar@gmail.com>
2023-03-13 21:17:38 -0600

    ¿¿¿¿

--- api/src/handlerBase.py
+++ api/src/handlerBase.py
@@ -6,45 +6,48 @@ import os
 
 import redis
 import json
+try:
+    class RedisDB:
+        def __init__(self, host, port, db, username, password):
+            self.host = host
+            self.port = port
+            self.db = db
+            self.username = username
+            self.password = password
+            self.pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.db, username=self.username, password=self.password)
+            
+        def set(self, key, value) -> None:
+            r = redis.Redis(connection_pool=self.pool)
+            r.set(key, value)
 
-class RedisDB:
-    def __init__(self, host, port, db, username, password):
-        self.host = host
-        self.port = port
-        self.db = db
-        self.username = username
-        self.password = password
-        self.pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.db, username=self.username, password=self.password)
+        def get(self, key) -> (bytes | None):
+            r = redis.Redis(connection_pool=self.pool)
+            return r.get(key)
         
-    def set(self, key, value) -> None:
-        r = redis.Redis(connection_pool=self.pool)
-        r.set(key, value)
+        def delete(self, key) -> None:
+            r = redis.Redis(connection_pool=self.pool)
+            r.delete(key)
 
-    def get(self, key) -> (bytes | None):
-        r = redis.Redis(connection_pool=self.pool)
-        return r.get(key)
-    
-    def delete(self, key) -> None:
-        r = redis.Redis(connection_pool=self.pool)
-        r.delete(key)
+        def exists(self, key) -> bool:
+            r = redis.Redis(connection_pool=self.pool)
+            return r.exists(key)
+        
+        def keys(self) -> list[bytes]:
+            r = redis.Redis(connection_pool=self.pool)
+            return r.keys()
+        
+        def flush(self) -> None:
+            r = redis.Redis(connection_pool=self.pool)
+            r.flushdb()
 
-    def exists(self, key) -> bool:
-        r = redis.Redis(connection_pool=self.pool)
-        return r.exists(key)
-    
-    def keys(self) -> list[bytes]:
-        r = redis.Redis(connection_pool=self.pool)
-        return r.keys()
-    
-    def flush(self) -> None:
-        r = redis.Redis(connection_pool=self.pool)
-        r.flushdb()
+        def get_all(self) -> dict:
+            return {key.decode("utf-8"): self.get(key).decode("utf-8") for key in self.keys()}
+        
+        def get_all_json(self) -> str:
+            return json.dumps(self.get_all())
 
-    def get_all(self) -> dict:
-        return {key.decode("utf-8"): self.get(key).decode("utf-8") for key in self.keys()}
-    
-    def get_all_json(self) -> str:
-        return json.dumps(self.get_all())
+except Exception as e:
+    print(e)
 
 # Singleton implementation for handler class
 class handlerBase(BaseHTTPRequestHandler):
