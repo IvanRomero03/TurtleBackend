97eafba56be01ab18b42c2031593f7720e1956ef
IvanRomero03 <i.wells.ar@gmail.com>
2023-03-14 18:04:07 -0600

    test

--- api/getSVG.py
+++ api/getSVG.py
@@ -14,8 +14,8 @@ class handler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Headers', '*')
-        self.send_header('Access-Control-Allow-Methods', '*')
+        #self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
         print(self.headers)
         print("OPTIONS")
         self.end_headers()
